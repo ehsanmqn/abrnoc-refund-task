9f5d173485edf3ebb6d75b70cfe99c7a2b45384d
Ehsan <spin313@gmail.com>
2024-07-25 20:36:51 +0330

    Refund blueprints have been added

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 from app.config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
+ma = Marshmallow()
 
 
 def create_app(config_class=Config):
@@ -14,5 +16,10 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    ma.init_app(app)
+
+    from app.blueprints.refunds import refunds_bp
+
+    app.register_blueprint(refunds_bp, url_prefix='/api')
 
     return app
