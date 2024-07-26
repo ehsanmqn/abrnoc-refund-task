78ec5aa056dd69477aec81e2f6a7252bc517730d
Ehsan <spin313@gmail.com>
2024-07-26 13:39:04 +0330

    Getting incomplete transactions and failed ones added

--- app/blueprints/transactions/routes.py
+++ app/blueprints/transactions/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from .services import get_transaction, list_transactions
+from .services import get_transaction, list_transactions, get_failed_transactions, get_incomplete_transactions
 
 transactions_bp = Blueprint('transactions', __name__)
 
@@ -20,3 +20,17 @@ def get_specific_transaction(transaction_id):
         return jsonify(transaction)
 
     return jsonify({'error': 'Transaction not found'}), 404
+
+
+@transactions_bp.route('/transactions/failed', methods=['GET'])
+def list_failed_transactions():
+    transactions = get_failed_transactions()
+
+    return jsonify(transactions)
+
+
+@transactions_bp.route('/transactions/incomplete', methods=['GET'])
+def list_incomplete_transactions():
+    transactions = get_incomplete_transactions()
+
+    return jsonify(transactions)
