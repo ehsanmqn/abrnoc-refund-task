c58277d3e336cce92ce907018e152f64a281bb6a
Ehsan <spin313@gmail.com>
2024-07-26 13:38:10 +0330

    get_incomplete_transactions and get_failed_transactions have been added

--- app/blueprints/transactions/services.py
+++ app/blueprints/transactions/services.py
@@ -4,6 +4,7 @@ from app.schemas import TransactionSchema
 
 def create_transaction(data):
     schema = TransactionSchema()
+
     transaction_data = schema.load(data)
     transaction = Transaction(**transaction_data)
 
@@ -29,7 +30,24 @@ def list_transactions(status_filter=None):
 
     if status_filter:
         query = query.filter_by(status=status_filter)
-
     transactions = query.all()
+
+    return schema.dump(transactions)
+
+
+def get_failed_transactions():
+    schema = TransactionSchema(many=True)
+
+    failed_statuses = ['failed']
+    transactions = Transaction.query.filter(Transaction.status.in_(failed_statuses)).all()
+
+    return schema.dump(transactions)
+
+
+def get_incomplete_transactions():
+    schema = TransactionSchema(many=True)
+
+    incomplete_statuses = ['pending', 'failed']
+    transactions = Transaction.query.filter(Transaction.status.in_(incomplete_statuses)).all()
 
     return schema.dump(transactions)
