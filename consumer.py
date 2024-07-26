62a1564ab40682728101271870b192e539d23740
Ehsan <spin313@gmail.com>
2024-07-26 11:30:43 +0330

    Topic updated

--- consumer.py
+++ consumer.py
@@ -1,18 +1,20 @@
 import json
 from kafka import KafkaConsumer
+
 from app import create_app, db
 from app.models import Transaction
 
 app = create_app()
 
 consumer = KafkaConsumer(
-    'refund_requests',
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers=app.config['KAFKA_BOOTSTRAP_SERVERS'],
     auto_offset_reset='earliest',
     enable_auto_commit=True,
     group_id='my-group',
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
+
+consumer.subscribe([app.config['REFUND_TOPIC']])
 
 with app.app_context():
     for message in consumer:
