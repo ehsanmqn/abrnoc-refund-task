c6afc9b5e4fa6b455aacb86e1e3e98d4e78f1daa
Ehsan <spin313@gmail.com>
2024-07-26 11:15:06 +0330

    Created at has been added to the tables

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from sqlalchemy.sql import func
 
 
 class Transaction(db.Model):
@@ -8,6 +9,7 @@ class Transaction(db.Model):
     payment_method = db.Column(db.String(64))
     description = db.Column(db.String(128))
     status = db.Column(db.String(64), default='pending')
+    created_at = db.Column(db.DateTime, server_default=func.now())
 
 
 class Refund(db.Model):
@@ -16,3 +18,4 @@ class Refund(db.Model):
     amount = db.Column(db.Float)
     reason = db.Column(db.String(128))
     status = db.Column(db.String(64), default='pending')
+    created_at = db.Column(db.DateTime, server_default=func.now())
