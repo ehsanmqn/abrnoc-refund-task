053d01786ef90247f27e75dd1766415a452811c6
Ehsan <spin313@gmail.com>
2024-07-26 12:17:18 +0330

    CLIs have been defined

--- manage.py
+++ manage.py
@@ -1,6 +1,25 @@
-from app import create_app
+import os
+from flask.cli import FlaskGroup
+from app import create_app, db
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+cli = FlaskGroup(app)
+
+
+@cli.command("create-db")
+def create_db():
+    with app.app_context():
+        db.create_all()
+    print("Database created.")
+
+
+@cli.command("drop-db")
+def drop_db():
+    with app.app_context():
+        db.drop_all()
+    print("Database dropped.")
+
+
+if __name__ == "__main__":
+    cli()
