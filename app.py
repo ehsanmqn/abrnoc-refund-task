d388ee67d3dbca337095e5d358e313864b228851
Ehsan <spin313@gmail.com>
2024-08-04 20:39:32 +0330

    Consumer changed to a Class

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from app import create_app, db
-from consumer import start_consumer_thread
+from consumer import KafkaConsumerService
 
 app = create_app()
+kafka_consumer = KafkaConsumerService(app)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
     with app.app_context():
-        start_consumer_thread(app)
+        kafka_consumer.start_consumer_thread()
 
     app.run(debug=True)
--- consumer.py
+++ consumer.py
@@ -2,53 +2,60 @@ import asyncio
 import json
 from threading import Thread
 from aiokafka import AIOKafkaConsumer
-from flask import current_app as app
 from concurrent.futures import ThreadPoolExecutor
-
 from app import db
 from app.models import Transaction
 
-executor = ThreadPoolExecutor(max_workers=10)
 
+class KafkaConsumerService:
+    def __init__(self, app):
+        self.app = app
+        self.executor = ThreadPoolExecutor(max_workers=10)
 
-async def handle_message(data):
-    transaction = Transaction(
-        transaction_id=data['transaction_id'],
-        amount=data['amount'],
-        description=data['description'],
-        payment_method=data['payment_method']
-    )
-    db.session.add(transaction)
+    async def handle_message(self, data):
+        with self.app.app_context():
+            try:
+                transaction = Transaction(
+                    user_id=data['user_id'],
+                    transaction_id=data['transaction_id'],
+                    amount=data['amount'],
+                    description=data['description'],
+                    payment_method=data['payment_method'],
+                    status=data['status']
+                )
+                db.session.add(transaction)
+                db.session.commit()
+                print(f"New transaction has been added to the database. Transaction ID: {data['transaction_id']}")
+            except Exception as e:
+                print(f"Error processing message: {e}")
+                db.session.rollback()
 
-    await asyncio.get_event_loop().run_in_executor(executor, db.session.commit)
+    async def consume_messages(self):
+        consumer = AIOKafkaConsumer(
+            self.app.config['REFUND_TOPIC'],
+            bootstrap_servers=self.app.config['KAFKA_BOOTSTRAP_SERVERS'],
+            group_id='my-group',
+            auto_offset_reset='earliest',
+            value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+        )
 
+        await consumer.start()
 
-async def consume_messages():
-    consumer = AIOKafkaConsumer(
-        app.config['REFUND_TOPIC'],
-        bootstrap_servers=app.config['KAFKA_BOOTSTRAP_SERVERS'],
-        group_id='my-group',
-        auto_offset_reset='earliest',
-        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-    )
+        try:
+            async for message in consumer:
+                await self.handle_message(message.value)
+        except Exception as e:
+            print(f"Error consuming messages: {e}")
+        finally:
+            await consumer.stop()
 
-    await consumer.start()
+    def start_consumer_loop(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
 
-    try:
-        async for message in consumer:
-            await handle_message(message.value)
-    finally:
-        await consumer.stop()
+        with self.app.app_context():
+            loop.run_until_complete(self.consume_messages())
 
-
-def start_consumer_loop(app):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    with app.app_context():
-        loop.run_until_complete(consume_messages())
-
-
-def start_consumer_thread(app):
-    t = Thread(target=start_consumer_loop, args=(app,))
-    t.start()
+    def start_consumer_thread(self):
+        t = Thread(target=self.start_consumer_loop)
+        t.start()
